080b50e34afd9ecf80c795536f6da3feabbb219a
lukecoopz <lukecoopz@gmail.com>
2024-03-06 15:47:48 +1300

    Update Pong2.py
    Added basic scoreboard to pong game

--- Pong2.py
+++ Pong2.py
@@ -1,6 +1,8 @@
 import pygame
+import pygame.font
 
 pygame.init()
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 600
 wn =pygame.display.set_mode((WIDTH, HEIGHT))
@@ -10,6 +12,10 @@ run = True
 #colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+#scoreboard
+left_score = 0
+right_score = 0
 
 #ball
 radius = 15
@@ -22,6 +28,8 @@ left_paddle_y = right_paddle_y = HEIGHT/2 - paddle_height/2
 left_paddle_x, right_paddle_x = 100 - paddle_width/2, WIDTH - (100 - paddle_height/2)
 right_paddle_vel = left_paddle_vel = 0 
 paddle_vel_constant = 0.3
+
+
 
 
 #main loop
@@ -83,10 +91,33 @@ while run:
     ball_y += ball_vel_y
     right_paddle_y += right_paddle_vel
     left_paddle_y += left_paddle_vel
+
+      #score
+    if ball_x >= WIDTH - radius:
+        ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 -radius
+        ball_vel_x *= -1
+        ball_vel_y *= -1
+        left_score += 1 # add a point to the right score
+
+    if ball_x <= 0 + radius:
+        ball_x, ball_y = WIDTH/2 - radius, HEIGHT/2 - radius
+        ball_vel_x, ball_vel_y = 0.12, 0.12
+        right_score += 1 # add a point to the left score
+
+    # create a font object
+    font = pygame.font.SysFont('Arial', 36)
+
+    # create a text surface
+    text_surface = font.render('{}: {}'.format(left_score, right_score), True, WHITE)
+
+    # draw the text surface on the screen
+    wn.blit(text_surface, (WIDTH/2 - text_surface.get_width()/2, 50))
     
     #OBJECTS
     pygame.draw.circle(wn, WHITE, (ball_x, ball_y), radius)
     pygame.draw.rect(wn, WHITE, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
     pygame.draw.rect(wn, WHITE, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
     pygame.display.update()
+
+  
 
